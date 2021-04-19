22b907f479fa925807a0c4f82966db153f1622ac
Srog1989 <s.rogers0715@yahoo.com>
2021-04-19 11:25:40 -0600

    setup Weather and Location Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Weather from './components/Weather'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        App
+       <Weather />
       </div>
     );
 
