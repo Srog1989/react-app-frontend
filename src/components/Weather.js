76ba0fd77d7b92edfd11aebf62ba5b0fc28bbc47
Srog1989 <s.rogers0715@yahoo.com>
2021-04-20 20:44:52 -0600

    refactored weather icon display and min max temp displays

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,27 @@
 import React from 'react'
 
-const Weather = () => {
+const Weather = (props) => {
     return(
     <div className="container">
      <div className="cards">
-      <h1>Miami</h1>
+      <h1>{props.city},{props.country}</h1>
       <h5 className="py-4">
-      <i className="wi wi-day-sunny">
-      </i> 
+      <i className={`wi ${props.weatherIcon} display-1`}/> 
       </h5>
+      <h1 className="py-2">{props.temp_farenheit}&deg;</h1>
+      {minmaxTemp(props.temp_min,props.temp_max)}
+      <h1 className="py-3">{props.description}</h1>
      </div>
     </div>
+    )
+}
+
+function minmaxTemp(min,max){
+    return(
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
     )
 }
 
