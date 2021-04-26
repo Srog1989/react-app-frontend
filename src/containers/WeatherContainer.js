19ccbf41a4bb64912d8e488a46af7a95ac5e597f
Srog1989 <s.rogers0715@yahoo.com>
2021-04-26 08:56:27 -0600

    modified css and package files

---
+++ src/containers/WeatherContainer.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Form from '../components/Form'
+import WeatherInfo from '../components/WeatherInfo'
+import "weather-icons/css/weather-icons.css"
+
+let weatherIcon={
+    Thunderstorm:"wi-thunderstorm",
+    Drizzle:"wi-sleet",
+    Rain:"wi-storm-showers",
+    Snow:"wi-snow",
+    Atmosphere:"wi-fog",
+    Clear:"wi-day-sunny",
+    Clouds:"wi-day-fog"
+  }
+
+
+const API_key="4215bb13a08c02af64771aeabee5aba7"
+
+class WeatherContainer extends Component {
+
+    // setBackground({icon}){
+    //     console.log(icon)
+    //     let body = document.querySelector("#body")
+    //     console.log(body)
+    //     if(icon === "wi-day-fog"){
+    //         body.setAttribute("class", "bg-cloudy")
+    //     }
+    // }
+    
+    calFarenheit(temp){
+        let faren=Math.floor(((temp - 273.15) * 1.8) + 32)
+        return faren
+      }
+
+    getWeather = async(e) => {
+        e.preventDefault()
+          const city = e.target.elements.city.value;
+          const country = e.target.elements.country.value;
+        if(city && country){
+          const api_call = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city},${country},&appid=${API_key}`
+            );
+            const response = await api_call.json()
+            console.log(response)
+            const weather = {
+                city: `${response.name},${response.sys.country}`,
+                farenheit: this.calFarenheit(response.main.temp),
+                temp_min: this.calFarenheit(response.main.temp_min),
+                temp_max: this.calFarenheit(response.main.temp_max),
+                description: response.weather[0].description 
+            };
+            this.props.setWeather(weather);
+            this.props.getWeatherIcon(weatherIcon, response.weather[0].id);
+        }else{
+            this.setState({error: true})
+        }
+    }
+
+    render() {
+        return (
+            <div className=".bg-img">
+                <Form loadweather={this.getWeather} error={this.props.error}/>
+                <WeatherInfo
+                    city={this.props.city}
+                    country={this.props.country} 
+                    temp_farenheit={this.props.farenheit}
+                    temp_max={this.props.temp_max}
+                    temp_min={this.props.temp_min}
+                    description={this.props.description}
+                    weatherIcon={this.props.icon}
+                />
+                
+            </div>
+        );
+    }
+}
+const mapStateToProps = ({ city, country, temp_farenheit, temp_max, temp_min, description, icon, error}) => ({ city, country, temp_farenheit, temp_max, temp_min, description, icon, error})
+
+const mapDispatchToProps = dispatch => ({
+    setWeather: weather => dispatch({type: "SET_WEATHER", weather }),
+    getWeatherIcon: (weatherIcon, rangeId) => dispatch({type: "GET_WEATHER_ICON", weatherIcon, rangeId })
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps) (WeatherContainer);
+
---
+++ src/reducers/manageWeather.js
@@ -0,0 +1,70 @@
+// import "weather-icons/css/weather-icons.css"
+
+// let weatherIcon={
+//     Thunderstorm:"wi-thunderstorm",
+//     Drizzle:"wi-sleet",
+//     Rain:"wi-storm-showers",
+//     Snow:"wi-snow",
+//     Atmosphere:"wi-fog",
+//     Clear:"wi-day-sunny",
+//     Clouds:"wi-day-fog"
+//   }
+
+
+export default function manageWeather(state =  {
+    city: undefined,
+    country: undefined,
+    icon: undefined,
+    main: undefined,
+    farenheit: undefined,
+    temp_max: undefined,
+    temp_min: undefined,
+    description: "",
+    error: false
+}, action)  {
+  switch (action.type) {
+    
+    case 'SET_WEATHER':
+      return { ...state,   
+        city: action.weather.city,
+        country: action.weather.country,
+        icon: action.weather.icon,
+        main: action.weather.main,
+        farenheit: action.weather.farenheit,
+        temp_max: action.weather.temp_max,
+        temp_min: action.weather.temp_min,
+        description: action.weather.description
+        // error: false
+    }
+    case 'GET_WEATHER_ICON':
+            if(action.rangeId >= 200&&action.rangeId <= 232){
+                return {...state, icon: action.weatherIcon.Thunderstorm}
+            }
+            else if (action.rangeId >= 300&&action.rangeId <= 321){
+                return {...state,icon:action.weatherIcon.Drizzle}
+            }
+            else if(action.rangeId >= 500&&action.rangeId <= 531){
+                return {...state, icon:action.weatherIcon.Rain}
+            }
+            else if(action.rangeId >= 600&&action.rangeId <= 622){
+                return {...state, icon:action.weatherIcon.Snow}
+            }
+            else if(action.rangeId >= 701&&action.rangeId <= 781){
+                return {...state, icon:action.weatherIcon.Atmosphere}
+            }
+            else if(action.rangeId >= 800&&action.rangeId <= 622){
+                return {...state, icon:action.weatherIcon.Snow}
+            }
+            else if(action.rangeId === 800){
+            return {...state, icon: action.weatherIcon.Clear}
+            }
+            else if(action.rangeId >= 801&&action.rangeId <= 804){
+                return {...state, icon:action.weatherIcon.Clouds}
+            }
+            else {return {...state, icon:action.weatherIcon.Clouds}
+          }
+      
+    default:
+      return state;
+  }
+};
