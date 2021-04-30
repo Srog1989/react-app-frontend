16e1192bfa127f5a716c4081954be17a5ab48ffe
Srog1989 <s.rogers0715@yahoo.com>
2021-04-29 20:44:40 -0600

    modified favorite component

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-
 const Favorite = (props) => {
 
 
     return (
-        <div>
-            <button onClick={()=> props.getWeather(props.location)}>
-                <Link to='/favorite-weather'>{props.location}</Link>
-            </button>
+        <div className="container">
+            <div className="card d-flex">
+                <button onClick={()=> props.getWeather(props.location)}>
+                    <Link to='/favorite-weather'>{props.location}</Link>
+                </button>
+            </div>
         </div>
+        
     );
 }
 
+
+
 export default Favorite;
+
